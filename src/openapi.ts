3de1715c4df316c04a8fc816b59fe6ccb0f7c832
purerosefallen <78877@qq.com>
2024-10-28 14:05:01 +0800

    add ApiTypeResponse and ApiBlankResponse

--- src/openapi.ts
+++ src/openapi.ts
@@ -1,5 +1,24 @@
-import { BlankReturnMessageDto } from './return-message';
-import { ApiResponse } from '@nestjs/swagger';
+import { BlankReturnMessageDto, ReturnMessageDto } from './return-message';
+import { ApiResponse, ApiResponseOptions } from '@nestjs/swagger';
+import { ClassOrArray } from './insert-field';
+
+export const ApiTypeResponse = (
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  type: ClassOrArray,
+  options: ApiResponseOptions = {},
+) =>
+  ApiResponse({
+    status: 200,
+    type: ReturnMessageDto(type),
+    ...options,
+  });
+
+export const ApiBlankResponse = (options: ApiResponseOptions = {}) =>
+  ApiResponse({
+    status: 200,
+    type: BlankReturnMessageDto,
+    ...options,
+  });
 
 export const ApiError = (status: number, description: string) =>
-  ApiResponse({ status, type: BlankReturnMessageDto, description });
+  ApiBlankResponse({ status, description });
