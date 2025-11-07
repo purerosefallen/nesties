d12c2f22eab07a36dbbb8789df3081dfd46b2dd7
purerosefallen <78877@qq.com>
2025-11-07 14:35:34 +0800

    update default value for i18n resolver

--- src/i18n-module/i18n-factory.ts
+++ src/i18n-module/i18n-factory.ts
@@ -1,10 +1,18 @@
-import { I18nModuleOptions } from './i18n-module.options';
+import { I18nModuleOptions, I18nResolver } from './i18n-module.options';
 import { createI18nDecorator } from './i18n-decorator';
 import { I18nModule } from './i18n.module';
 
-export const createI18n = (options: I18nModuleOptions) => {
+export const createI18n = (
+  options: Omit<I18nModuleOptions, 'resolver'> & { resolver?: I18nResolver },
+) => {
+  if (!options.resolver) {
+    options.resolver = {
+      paramType: 'header',
+      paramName: 'x-client-language',
+    };
+  }
   return {
-    UseI18n: createI18nDecorator(options),
-    I18nModule: I18nModule.register(options),
+    UseI18n: createI18nDecorator(options as I18nModuleOptions),
+    I18nModule: I18nModule.register(options as I18nModuleOptions),
   };
 };
