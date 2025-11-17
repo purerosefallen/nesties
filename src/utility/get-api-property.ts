2819ef8ae393c39319e44cfc2e70f66a00f55328
purerosefallen <78877@qq.com>
2025-11-17 18:00:14 +0800

    getApiProperty by prototype

--- src/utility/get-api-property.ts
+++ src/utility/get-api-property.ts
@@ -6,8 +6,19 @@ export const getApiProperty = (
   cls: AnyClass,
   key: string,
 ): ApiPropertyOptions => {
-  return (
-    Reflect.getMetadata(DECORATORS.API_MODEL_PROPERTIES, cls.prototype, key) ||
-    {}
-  );
+  let proto = cls.prototype;
+
+  while (proto && proto !== Object.prototype) {
+    const meta = Reflect.getMetadata(
+      DECORATORS.API_MODEL_PROPERTIES,
+      proto,
+      key,
+    );
+
+    if (meta) return meta;
+
+    proto = Object.getPrototypeOf(proto);
+  }
+
+  return {};
 };
